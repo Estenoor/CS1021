c4315e1a4445c993d32f2a2128b74108d7d192b5
Sam <SamzFerg@gmail.com>
2019-12-03 14:45:10 -0600

    Added more functionality to ReverseSample and GenerateSimpleWavFile

--- src/msoe/fergusons/lab01/WaveGenerator.java
+++ src/msoe/fergusons/lab01/WaveGenerator.java
@@ -20,32 +20,60 @@ public class WaveGenerator {
 
     private static Scanner input;
 
+    private static final long NUM_FRAMES = 8000;
+    private static final int VALID_BITS = 8;
+    private static final long SAMPLE_RATE = 8000;
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int command = getUserCommand(input);
         boolean exitProgram = false;
         do {
             switch(command) {
-                case 0:
+                case 0: //Exit the Program
                     exitProgram = true;
                     break;
-                case 1:
+                case 1: //Reverse Given WavFile
                     String fileName = getFileName(input);
                     WavFile wavFile = new WavFile(fileName + ".wav");
                     ArrayList<Double> audioSamples = wavFile.getSamples();
-                    ArrayList<Double> reversedSamples = reverseWavFile(audioSamples);
+                    // Reverses Audio Samples from Original Wave File
+                    ArrayList<Double> reversedSamples = reverseWavFileSamples(audioSamples);
+                    // Close original Wav File
+                    if(wavFile.close()) {
+                        System.out.println("Original File CLosed Successfully");
+                    } else {
+                        System.out.println("Original File Not Closed Properly");
+                    }
+
+                    // Generate new WavFile object for reversed sample
+                    WavFile reversedWavFile = new WavFile(fileName + "Rev.wav",
+                            wavFile.getNumChannels(),
+                            wavFile.getNumFrames(),
+                            wavFile.getValidBits(),
+                            wavFile.getSampleRate());
+
+                    // Saves Reversed audio samples to new file.
+                    reversedWavFile.setSamples(reversedSamples);
+                    // Closes Reversed Wav File and releases resources
+                    if(reversedWavFile.close()) {
+                        System.out.println("Reversed Wave File Saved Successfully!");
+                    } else {
+                        System.out.println("Reversed Wave File Not Saved Correctly");
+                    }
+                    break;
+                case 2: // Generate Simple WavFile
+                    WavFile simpleWavFile = generateSimpleWavFile();
 
                     break;
-                case 2:
-                    WavFile simpleWavFile = generateSimpleWavFile();
-                    break;
-                case 3:
+                case 3: // Generate Stereo File
                     WavFile stereoWavFile = generateStereoWavFile();
                     break;
+                default: // Continue asking for user input
+                    command = getUserCommand(input);
             }
 
-        }
-        while(!exitProgram);
+        } while(!exitProgram);
     }
 
     /**
@@ -83,7 +111,7 @@ public class WaveGenerator {
      * @param audioSamples the audio samples to reverse
      * @return an arraylist of the reversed audio samples
      */
-    private static ArrayList<Double> reverseWavFile(ArrayList<Double> audioSamples) {
+    private static ArrayList<Double> reverseWavFileSamples(ArrayList<Double> audioSamples) {
         ArrayList<Double> reversedSamples = new ArrayList<Double>();
         for (int i = audioSamples.size() - 1; i >= 0; i--) {
             reversedSamples.add(audioSamples.get(i));
@@ -91,15 +119,46 @@ public class WaveGenerator {
         return reversedSamples;
     }
 
-    public static WavFile generateSimpleWavFile() {
+    /**
+     * Generates a simple wave file from a given frequency
+     * @param frequency The frequency to use when creating the wave file.
+     * @param fileName The name of the audio file to create
+     * @return The Generated WavFile Object
+     */
+    public static void generateSimpleWavFile(double frequency, String fileName) {
+        int numChannels = 1;
+        WavFile simpleWavFile = new WavFile(fileName + ".wav",
+                numChannels,
+                NUM_FRAMES,
+                VALID_BITS,
+                SAMPLE_RATE);
+
+        ArrayList<Double> audioSamples = new ArrayList<Double>();
+        for(int i = 0; i < SAMPLE_RATE; i++) {
+            audioSamples.add(generateSineWaveValues(frequency, i));
+        }
+        simpleWavFile.setSamples(audioSamples);
+        // Closes Simple Wave FIle
+        simpleWavFile.close();
+    }
+
+    /**
+     * Generates a simple wave file in stereo
+     * @return The Generated WavFile Object
+     */
+    public static WavFile generateStereoWavFile() {
         //placeholder
 
         return null;
     }
 
-    public static WavFile generateStereoWavFile() {
-        //placeholder
-
-        return null;
+    /**
+     * Simple function to generate each sample from the frequency and sample index
+     * @param frequency The frequency of sound to generate
+     * @param sample The index of the sample to generate
+     * @return returns the requested audio sample
+     */
+    public static Double generateSineWaveValues(double frequency, double sample) {
+        return (2 * Math.PI) * sample * (frequency / SAMPLE_RATE);
     }
 }
